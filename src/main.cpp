5b319ff852734d2da8d63b45cd27f5131385178b
MCMrARM <mcmrarm@gmail.com>
2018-06-15 21:08:54 +0200

    Warn if directory is not empty

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
 #include <cstdio>
 #include <mcpelauncher/zip_extractor.h>
 #include <mcpelauncher/minecraft_extract_utils.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 #define CLEAR_LINE "\033[2K\r"
+
+static void checkFolder(std::string const& path) {
+    struct stat info;
+    if (stat(path.c_str(), &info) < 0) {
+        if (errno == ENOENT)
+            return;
+        printf("Warning: stat() failed on the specified destination\n");
+        return;
+    }
+    if (!(info.st_mode & S_IFDIR)) {
+        printf("The specified path exists and is not a directory\n");
+        exit(1);
+    }
+    DIR* dir = opendir(path.c_str());
+    if (!dir) {
+        printf("Failed to open the output directory\n");
+        exit(1);
+    }
+    struct dirent* de;
+    while (de = readdir(dir)) {
+        if (de->d_name[0] == '.')
+            continue;
+        printf("Warning: Directory is not empty\n");
+        break;
+    }
+    closedir(dir);
+}
 
 int main(int argc, const char* argv[]) {
     if (argc != 3) {
@@ -12,6 +41,8 @@ int main(int argc, const char* argv[]) {
 
     std::string inPath = argv[1];
     std::string outPath = argv[2];
+
+    checkFolder(outPath);
 
     ZipExtractor extractor (inPath);
     int lastPercentageReported = -1;
